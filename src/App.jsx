c0e2a126f672252da96c77ea273d04ccb0395b41
JMartin Pacheco <martinpacheco1192@gmail.com>
2025-05-14 22:01:25 -0600

    base

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import imagen from '/QR_mensaje.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={imagen} className="logo" alt=" logo" />
+      </div>
+      <p className="read-the-docs">
+        
+      </p>
+    </>
+  )
+}
+
+export default App
